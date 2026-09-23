995dce0dac09897cf8497d6efab56821f78b830d
santiagopineda0353 <santiagopineda0353@gmail.com>
2026-09-23 16:01:02 -0500

    HU14 Test Unitarios

---
+++ src/test/java/com/pragma/powerup/domain/usecase/TraceabilityUseCaseTest.java
@@ -0,0 +1,39 @@
+package com.pragma.powerup.domain.usecase;
+
+import com.pragma.powerup.domain.model.TraceabilityModel;
+import com.pragma.powerup.domain.spi.ITraceabilityPersistencePort;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class TraceabilityUseCaseTest {
+
+    @Mock
+    private ITraceabilityPersistencePort traceabilityPersistencePort;
+
+    @InjectMocks
+    private TraceabilityUseCase traceabilityUseCase;
+
+    private TraceabilityModel validTraceability;
+
+    @BeforeEach
+    void setUp(){
+        validTraceability= new TraceabilityModel(null,1L,5L,"cliente@correo.com",
+                null,"PENDIENTE","EN_PREPARACION",3L,"empleado@correo.com");
+    }
+
+    @Test
+    void saveTraceability_whenValidData_thenAssignDateAndSave(){
+        traceabilityUseCase.saveTraceability(validTraceability);
+        assertNotNull(validTraceability.getDate());
+        verify(traceabilityPersistencePort, times(1)).saveTraceability(validTraceability);
+    }
+}
